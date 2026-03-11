ea720a84fd4689fc31ad67474871faaf35a5328f
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2026-03-11 14:57:22 +0100

    Revert "(ref) : inlining - remplacent de l'appel à la méthode isDateBetween par son code (sans suppression) dans la méthode isNowBetween dans la classe DateUtils"
    This reverts commit b4d29f44452230c61be114f2a3e9ab187aca437d.

--- tutoRefactoring/src/main/java/DateUtils.java
+++ tutoRefactoring/src/main/java/DateUtils.java
@@ -2,10 +2,7 @@ import java.time.LocalDate;
 
 public class DateUtils {
     public static boolean isNowBetween(LocalDate startingDate, LocalDate endingDate) {
-        LocalDate now = LocalDate.now();
-        return now.isAfter(startingDate)
-                &&
-                now.isBefore(endingDate);
+        return isDateBetween(startingDate, endingDate, LocalDate.now());
     }
 
     private static boolean isDateBetween(LocalDate startingDate, LocalDate endingDate, LocalDate now) {
