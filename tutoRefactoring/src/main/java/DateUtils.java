e10b3ffdcdfaf52f32db90a175350bbff25ab685
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2026-03-11 15:11:06 +0100

    (ref) : changement de signature de la méthode isDateBetween dans la classe DateUtils

--- tutoRefactoring/src/main/java/DateUtils.java
+++ tutoRefactoring/src/main/java/DateUtils.java
@@ -2,17 +2,34 @@ import java.time.LocalDate;
 
 public class DateUtils {
     public static boolean isNowBetween(LocalDate startingDate, LocalDate endingDate) {
-        return isDateBetween(startingDate, endingDate, LocalDate.now());
+        return isDateBetween(startingDate, endingDate, LocalDate.now(), true);
     }
 
-    private static boolean isDateBetween(LocalDate startingDate, LocalDate endingDate, LocalDate now) {
-        return now.isAfter(startingDate)
+    protected static boolean isDateBetween(LocalDate startingDate, LocalDate endingDate, LocalDate
+            now, boolean inclusive) {
+        LocalDate startingDay; // sera le paramètre startingDate ou le lendemain du paramètre
+        LocalDate endingDay; // sera le paramètre endingDate ou le la veille du paramètre
+        if (inclusive) {
+            startingDay = startingDate.minusDays(1); // la veille
+            endingDay = startingDate.plusDays(1); // le lendemain
+        }
+        else {
+            startingDay = startingDate;
+            endingDay = startingDate;
+        }
+        return now.isAfter(startingDay)
                 &&
-                now.isBefore(endingDate);
+                now.isBefore(endingDay);
+         /* Pour info :
+         * date1.isBefore(date1) retourne FAUX // Before strict
+         * date1.isAfter(date1) retourne FAUX // After strict
+         * Donc, pour que l'évaluation soit inclusive,
+         * il faut calculer After(laVeille du paramètre staringDate) et Before(le lendemain du
+        paramètre endingDate) */
     }
 
     private static boolean isDateOutsider(LocalDate date, LocalDate startingDate, LocalDate
             endingDate) {
-        return !isDateBetween(date, startingDate, endingDate);
+        return !isDateBetween(date, startingDate, endingDate, true);
     }
 }
